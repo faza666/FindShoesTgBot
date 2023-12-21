050bcd65ef3301b804a64ceb6d3ca92e3abca1f9
faza666 <faza666@gmail.com>
2023-12-21 19:13:53 +0200

    Webhook implemented

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -1,19 +1,23 @@
+from aiogram import Router
 from aiogram.types import Message
 from core.keyboards.main_keyboard import get_main_keyboard
 from core.utils.parse_call import find_shoes as my_shoes
 
+router = Router()
 
+
+@router.message()
 async def get_start(message: Message):
     await message.answer(f'Привіт, кохана! 😍 \nГотова до шопингу? )',
                          reply_markup=get_main_keyboard())
 
 
+@router.message()
 async def find_shoes(message: Message):
     await message.answer('Шукаємо...')
 
     shoes_cards = await my_shoes()
     for each_answer in shoes_cards:
         await message.answer(
-            each_answer,
-            parse_mode='HTML'
+            each_answer
         )
--- main.py
+++ main.py
@@ -1,38 +1,58 @@
-from aiogram import Bot, Dispatcher, F
-import asyncio
-from core.handlers.basic import get_start, find_shoes
-from core.settings import settings
+from aiogram import Bot, Dispatcher, F, types
+from aiogram.enums import ParseMode
+from core.handlers.basic import router, get_start, find_shoes
 import logging
+from aiohttp import web
+
+from core.settings import settings
 from core.utils.commands import set_commands
 
+BASE_WEBHOOK_URL = 'https://fcbd-193-34-172-202.ngrok-free.app'
+API_TOKEN = settings.bots.bot_token
 
-async def start_bot(bot: Bot):
+
+async def on_startup(_):
     await set_commands(bot)
     await bot.send_message(settings.bots.admin_id, 'Bot has started')
+    await bot.set_webhook(
+        f"{BASE_WEBHOOK_URL}/{API_TOKEN}"
+    )
 
 
-async def stop_bot(bot: Bot):
+async def on_shutdown(_):
     await bot.send_message(settings.bots.admin_id, 'Bot has stopped')
 
 
-async def start():
-    logging.basicConfig(level=logging.INFO,
-                        format="%(asctime)s - [%(levelname)s] - %(name)s"
-                               "(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
-                        )
-    bot = Bot(token=settings.bots.bot_token)
-    dp = Dispatcher()
-
-    dp.message.register(get_start, F.text == '/start')
-    dp.message.register(find_shoes, F.text == 'Знайти шузи')
-    dp.startup.register(start_bot)
-    dp.shutdown.register(stop_bot)
-
-    try:
-        await dp.start_polling(bot)
-    finally:
-        await bot.session.close()
+async def catch_webhook(request: web.Request):
+    token = str(request.url).split('/')[-1]
+    if token == API_TOKEN:
+        request_data = await request.json()
+        update = types.Update(**request_data)
+        await dp._process_update(update=update, bot=bot)
+        return web.Response()
+    else:
+        return web.Response(status=403)
 
 
 if __name__ == '__main__':
-    asyncio.run(start())
+    logging.basicConfig(
+        level=logging.INFO,
+        format=("%(asctime)s - [%(levelname)s] - %(name)s"
+                "(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s")
+    )
+    bot = Bot(token=settings.bots.bot_token, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_router(router=router)
+
+    # dp.message.register(get_start, F.text == '/start')
+    dp.message.register(find_shoes, F.text == 'Знайти шузи')
+
+    app = web.Application()
+    app.router.add_post(f'/{API_TOKEN}', catch_webhook)
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+    web.run_app(
+        app,
+        host='0.0.0.0',
+        port=8888
+    )
